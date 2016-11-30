8d0cc4e2bccf5f08f1c4c0b422f692366f6953ad
Pedro León <pedro.leon@wearestrings.com>
2016-11-30 17:45:03 +0100

    GET-13
    - Created a CensusApiCustom class to integrate RESTFULL census ws provided
    - Custom structure
    - Use specified schema to customize Consul with custom verification/residence

--- app/models/custom/verification/residence.rb
+++ app/models/custom/verification/residence.rb
@@ -3,18 +3,18 @@ require_dependency Rails.root.join('app', 'models', 'verification', 'residence')
 
 class Verification::Residence
 
-  validate :postal_code_in_madrid
-  validate :residence_in_madrid
+  validate :postal_code_in_getafe
+  validate :residence_in_getafe
 
-  def postal_code_in_madrid
+  def postal_code_in_getafe
     errors.add(:postal_code, I18n.t('verification.residence.new.error_not_allowed_postal_code')) unless valid_postal_code?
   end
 
-  def residence_in_madrid
+  def residence_in_getafe
     return if errors.any?
 
     unless residency_valid?
-      errors.add(:residence_in_madrid, false)
+      errors.add(:residence_in_getafe, false)
       store_failed_attempt
       Lock.increase_tries(user)
     end
@@ -23,7 +23,16 @@ class Verification::Residence
   private
 
     def valid_postal_code?
-      postal_code =~ /^280/
+      postal_code =~ /^289/
     end
 
+    def residency_valid?
+        @census_api_response.valid? &&
+        @census_api_response.postal_code == postal_code &&
+        @census_api_response.date_of_birth == date_of_birth
+    end
+
+    def call_census_api
+      @census_api_response = CensusApiCustom.new.call(document_type, document_number)
+    end
 end
