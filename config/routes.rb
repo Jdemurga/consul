2ebf98255fe8fe9dd5cc36dd5ca9c495d92cc294
pedroleon <pedro.leon@wearestrings.com>
2016-12-20 21:24:24 +0100

    GET-39

--- config/routes.rb
+++ config/routes.rb
@@ -279,7 +279,7 @@ Rails.application.routes.draw do
   # GET-22
   get 'participatory_budget/in_two_minutes', to: 'pages#show', id: 'participatory_budget/in_two_minutes'
   get 'participatory_budget', to: 'spending_proposals#welcome', as: 'participatory_budget'
-  get 'more_information', to: 'pages#show', as: 'mode_information', id: 'participatory_budget_info'
+  get 'more_information', to: 'pages#show', as: 'mode_information', id: 'more_information'
   get 'about_neighborhood_commissions', to: 'pages#show', as: 'sobre-comisiones-barrio', id: 'about_neighborhood_commissions'
 
   resources :pages, path: '/', only: [:show]
