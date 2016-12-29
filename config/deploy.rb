08309895b233ce36420b752b30e1c28add6fe15d
pedroleon <pedro.leon@wearestrings.com>
2016-12-29 21:35:18 +0100

    GET-50
    Capistrano script updated to production

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :log_level, :info
 set :pty, true
 set :use_sudo, false
 
-set :linked_files, %w{config/database.yml config/secrets.yml config/puma_staging.rb config/application_custom.rb}
+set :linked_files, %w{config/database.yml config/secrets.yml config/puma.rb config/application_custom.rb}
 set :linked_dirs, %w{log tmp public/system public/assets}
 
 set :keep_releases, 5
