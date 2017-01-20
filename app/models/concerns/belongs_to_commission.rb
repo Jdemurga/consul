e42002b6d4f176c7dde09594d81348023d406a73
pedroleon <pedro.leon@wearestrings.com>
2017-01-20 13:51:06 +0100

    Fixed

--- app/models/concerns/belongs_to_commission.rb
+++ app/models/concerns/belongs_to_commission.rb
@@ -8,7 +8,7 @@ module BelongsToCommission
 
 
   def validate_commission_can_not_be_updated_twice
-    errors.add(:commission, I18n.t('errors.commission_can_not_be_updated_twice')) unless commission_id_was.nil?
+    errors.add(:commission, I18n.t('errors.commission_can_not_be_updated_twice')) unless commission_id_was.eql?(commission_id)
   end
 
   class_methods do
