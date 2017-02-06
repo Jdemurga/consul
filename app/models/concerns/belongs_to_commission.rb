44f4684089832c0441bf646ea670fe0220f3c7ef
pedroleon <pedro.leon@wearestrings.com>
2017-02-06 13:41:48 +0100

    Fix comission inscription button

--- app/models/concerns/belongs_to_commission.rb
+++ app/models/concerns/belongs_to_commission.rb
@@ -8,6 +8,7 @@ module BelongsToCommission
 
 
   def validate_commission_can_not_be_updated_twice
+    return true if commission_id_was.nil?
     errors.add(:commission, I18n.t('errors.commission_can_not_be_updated_twice')) unless commission_id_was.eql?(commission_id)
   end
 
