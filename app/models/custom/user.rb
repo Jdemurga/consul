b844fe6ca62e14d7005a7017588ee9a245109da9
pedroleon <pedro.leon@wearestrings.com>
2017-05-18 14:28:18 +0200

    Added  before_save  to normalize

--- app/models/custom/user.rb
+++ app/models/custom/user.rb
@@ -2,6 +2,8 @@ require_dependency Rails.root.join('app', 'models', 'user').to_s
 
 class User
 
+  before_save :normalize_email
+
   def normalize_email
     return unless self.email
     if self.email.include?('+') && self.email.include?('@')
