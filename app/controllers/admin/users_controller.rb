b0c7334c1dccaba602cabad14d15b26a3649947a
pedroleon <pedro.leon@wearestrings.com>
2018-01-15 10:16:30 +0100

    Sort by creation date

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,7 +6,7 @@ class Admin::UsersController < Admin::BaseController
       s = params[:search]
       @users = User.where("username ILIKE ? OR email ILIKE ? OR document_number ILIKE ?", "%#{s}%", "%#{s}%", "%#{s}%").page(params[:page])
     else
-      @users = @users.page(params[:page])
+      @users = @users.order(created_at: 'asc').page(params[:page])
     end
     respond_to do |format|
       format.html
